0cdf676721a1c585bac2a7a0979c7224d92291ba
AtulMinz <atulminz.jsr@gmail.com>
2024-01-07 23:27:40 +0530

    Added Docker File

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use(morgan());
+app.use(morgan("combined"));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
